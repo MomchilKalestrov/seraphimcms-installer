027e63b3a5e6ef9b8d82569a21a951d7574c0c3f
Momchil Kalestrov <kalestrov.mo@gmail.com>
2026-01-21 13:58:06 +0200

    add windows support

--- src/dockerInstallers/win32.ts
+++ src/dockerInstallers/win32.ts
@@ -1,6 +1,23 @@
+import fs from 'node:fs';
+import http from 'node:https';
+
+import { run } from '../utils/exec.ts';
+
+const FILENAME = '.\\DockerDesktopInstaller.exe';
+
+const downloadDockerDesktop = (): Promise<void> =>
+    new Promise(async resolve => {
+        http.get('https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe', response => {
+            const writeStream = fs.createWriteStream(FILENAME);
+            writeStream.on('finish', resolve);
+            response.pipe(writeStream);
+        });
+    });
+
 const installDocker = async () => {
-    console.error('Script must be executed within a WSL2 or Hyper-V environment.');
-    process.exit(0);
+    await downloadDockerDesktop();
+
+    run(FILENAME, [ 'install', '--accept-license', '--backend=wsl-2', '--always-run-service' ]);
 };
 
 export default installDocker;
