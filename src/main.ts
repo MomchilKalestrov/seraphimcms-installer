673f1d5a14159a73e160e01496648a1b1dbee21d
Momchil Kalestrov <kalestrov.mo@gmail.com>
2026-01-29 19:23:38 +0200

    replace `crypto.argon2` with a node module since qode isn't up to date and doesn't have `crypto.argon2`

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import Window from './window/index.ts';
 
 new Window();
+
+// process.on('uncaughtException', error => {
+//     console.log('Unhandled exception thrown: ' + error.message);
+//     console.log('At:');
+//     console.log(error.stack);
+// });
+
 (async () => void 0)();
--- src/pages/createUser/createOwnerUser.ts
+++ src/pages/createUser/createOwnerUser.ts
@@ -1,35 +1,10 @@
-import crypto from 'node:crypto';
-
+import argon2 from 'argon2';
 import { MongoClient } from 'mongodb';
-
-
-const hashPassword = async (password: string): Promise<string> => {
-    const algorithm = 'argon2id';
-    const nonce = crypto.randomBytes(16);
-    const parallelism = 4;
-    const tagLength = 32;
-    const memory = 65536;
-    const passes = 3;
-    
-    const hash = crypto.argon2Sync(algorithm, {
-        message: password,
-        nonce,
-        parallelism,
-        tagLength,
-        memory,
-        passes
-    });
-    
-    //@ts-ignore
-    const passwordHash = `$${ algorithm }$v=19$m=${ memory },t=${ passes },p=${ parallelism }$${ nonce.toBase64({ omitPadding: true }) }$${ hash.toBase64({ omitPadding: true }) }`;
-    
-    return passwordHash;
-};
 
 const createOwnerUser = async (username: string, password: string) => {
     const user = {
         username,
-        passwordHash: hashPassword(password),
+        passwordHash: argon2.hash(password),
         role: 'owner'
     };
     
