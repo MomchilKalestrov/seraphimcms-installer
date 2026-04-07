7b60b868c17d8ff093e649edf6ae476cebf03411
Momchil Kalestrov <kalestrov.mo@gmail.com>
2026-04-07 19:45:06 +0300

    refactor: change where zstd is cloned

--- sea/scripts/build.sh
+++ sea/scripts/build.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
+set -e
 
-BASEDIR=$(dirname $0)/..
+BASE_DIR=$(dirname $0)/..
 PAYLOAD_PATH=$1
 GCC_PREFIX=$2
-ZSTD_PATH=$BASEDIR/lib/zstd/lib/libzstd.a
+ZSTD_PATH=$BASE_DIR/lib/zstd/lib/libzstd.a
 
-mkdir -p $BASEDIR/build
+mkdir -p $BASE_DIR/build
 
 function prepare_payload_windows {
     ${GCC_PREFIX}objcopy -I binary -O pe-x86-64 -B i386:x86-64 \
         --rename-section .data=.payload,contents,alloc,load,readonly,data \
-        $PAYLOAD_PATH $BASEDIR/build/payload.o
+        $PAYLOAD_PATH $BASE_DIR/build/payload.o
 }
 
 function prepare_payload_linux {
     ${GCC_PREFIX}objcopy -I binary -O elf64-x86-64 -B i386:x86-64 \
         --rename-section .data=.payload,contents,alloc,load,readonly,data \
-        $PAYLOAD_PATH $BASEDIR/build/payload.o
+        $PAYLOAD_PATH $BASE_DIR/build/payload.o
 }
 
-function build_zstd {
-    make -C $BASEDIR/lib/zstd/lib ZSTD_LIB_COMPRESSION=0 ZSTD_LIB_DECOMPRESSION=1 CC=${GCC_PREFIX}gcc AR=${GCC_PREFIX}ar
+function prepare_zstd {
+    file $BASE_DIR/lib/zstd > /dev/null 2> /dev/null
+    if [ $? = 1 ]; then
+        mkdir -p $BASE_DIR/lib/zstd
+        git clone https://github.com/facebook/zstd.git $BASE_DIR/lib/zstd
+    fi
+    make -C $BASE_DIR/lib/zstd/lib ZSTD_LIB_COMPRESSION=0 ZSTD_LIB_DECOMPRESSION=1 CC=${GCC_PREFIX}gcc AR=${GCC_PREFIX}ar
 }
 
 function build_binary { 
     ${GCC_PREFIX}gcc \
-        $BASEDIR/src/data/data.c \
-        $BASEDIR/src/mkdir/mkdir.c \
-        $BASEDIR/src/main.c \
-        $ZSTD_PATH $ICON_OBJECT $BASEDIR/build/payload.o \
-        -T $BASEDIR/src/linker.ld \
-        -o $BASEDIR/build/out
+        $BASE_DIR/src/data/data.c \
+        $BASE_DIR/src/mkdir/mkdir.c \
+        $BASE_DIR/src/main.c \
+        $ZSTD_PATH $ICON_OBJECT $BASE_DIR/build/payload.o \
+        -T $BASE_DIR/src/linker.ld \
+        -o $BASE_DIR/build/out
 }
 
 function generate_icon_windows {
-    cat > $BASEDIR/build/icon.rc <<< "1 ICON \"$BASEDIR/src/icon.ico\""
-    ${GCC_PREFIX}windres $BASEDIR/build/icon.rc -O coff -o $BASEDIR/build/icon.o
-    echo $BASEDIR/build/icon.o
+    cat > $BASE_DIR/build/icon.rc <<< "1 ICON \"$BASE_DIR/src/icon.ico\""
+    ${GCC_PREFIX}windres $BASE_DIR/build/icon.rc -O coff -o $BASE_DIR/build/icon.o
+    echo $BASE_DIR/build/icon.o
 }
 
 echo Building ZSTD
