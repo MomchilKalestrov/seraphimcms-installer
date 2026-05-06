ff3194acd88cf357baa72b94c46e0df6b953413d
Momchil Kalestrov <kalestrov.mo@gmail.com>
2026-05-06 16:07:29 +0300

    fix(constants): rewrite `ENV_PATH` with a platform-agnostic value

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,9 @@
+import os from 'node:os';
+import path from 'node:path';
+
 export const DOCKER_BIN = '/usr/bin/docker';
 export const CONTAINER_NAME = 'seraphimcms';
-export const ENV_PATH = `/etc/${ CONTAINER_NAME }`;
+export const ENV_PATH = path.resolve(os.homedir(), 'seraphimcms');
 export const ENV_FILE = `${ ENV_PATH }/.env`;
 export const IMAGE_NAME = 'seraphimcms:latest';
 export const OWNER = 'MomchilKalestrov';
